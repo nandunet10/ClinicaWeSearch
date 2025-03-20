ff67b362b28f92f5ef39152c25abddfcb1d2373b
susepbr\ba-fernandoos <fernando.santos@susep.gov.br>
2025-03-20 11:56:44 -0300

    ajustes

--- Frontend/SistemaSaude.Webapp/src/app/shared/components/nav-menu/nav-menu.component.ts
+++ Frontend/SistemaSaude.Webapp/src/app/shared/components/nav-menu/nav-menu.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
-
-interface MenuItem {
-  label: string;
-  url: string;
-  subItems?: MenuItem[];
-}
+import { IMenuItem } from '../../models/menu.item.model';
 
 @Component({
   selector: 'app-nav-menu',
@@ -14,11 +9,18 @@ interface MenuItem {
   styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent implements OnInit {
-  menuItems: MenuItem[] = [
-    { label: 'Atendimentos', url: '/atendimentos' },
-    { label: 'Pacientes', url: '/pacientes' },
-    { label: 'Cadastro de Paciente', url: '/cadastro-paciente' },
-    { label: 'Cadastro de Atendimento', url: '/cadastro-atendimento' },
+  menuItems: IMenuItem[] = [
+    { text: 'Dashboard', url: '', isOpen: false },
+    {
+      text: 'Atendimentos', url: '/atendimentos', isOpen: false, subItems: [
+        { text: 'Cadastro de Atendimento', url: '/cadastro-atendimento', isOpen: false }
+      ]
+    },
+    {
+      text: 'Pacientes', url: '/pacientes', isOpen: false, subItems: [
+        { text: 'Cadastro de Paciente', url: '/cadastro-paciente', isOpen: false }
+      ]
+    }
   ];
 
   isMenuOpen: boolean = false;
@@ -32,6 +34,11 @@ export class NavMenuComponent implements OnInit {
     return exact ? currentUrl === url : currentUrl.startsWith(url);
   }
 
+  toggleSubItems(menuItem: IMenuItem): void {
+    if (menuItem.subItems) {
+      menuItem.isOpen = !menuItem.isOpen;
+    }
+  }
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
   }
