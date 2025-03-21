72a439dcda9e79f4e8901f8e622f490ef312b603
susepbr\ba-fernandoos <fernando.santos@susep.gov.br>
2025-03-21 01:08:29 -0300

    Add step 2 of standalone.

--- Frontend/SistemaSaude.Webapp/src/app/app.component.ts
+++ Frontend/SistemaSaude.Webapp/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { BreadcrumbComponent } from './shared/components/breadcrumb/breadcrumb.component';
+import { NavMenuComponent } from './shared/components/nav-menu/nav-menu.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: false,
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [BreadcrumbComponent, NavMenuComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'Sistema de Atendimento';
--- Frontend/SistemaSaude.Webapp/src/main.ts
+++ Frontend/SistemaSaude.Webapp/src/main.ts
@@ -1,5 +1,20 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { SharedModule } from './app/shared/shared.module';
+import { PacientesModule } from './app/modules/pacientes/pacientes.module';
+import { AtendimentosModule } from './app/modules/atendimentos/atendimentos.module';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, SharedModule, PacientesModule, AtendimentosModule),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideRouter(routes)
+    ]
+})
   .catch(err => console.error(err));
